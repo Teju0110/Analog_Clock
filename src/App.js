249cac889baf64bc2a3cb811b75751d6c9aaabaf
tejasvisawant1998@gmail.com <tejasvisawant1998@gmail.com>
2022-09-15 13:52:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const deg = 6;
+
+  const [hh, setHh] = useState();
+  const [min, setMin] = useState();
+  const [sec, setSec] = useState();
+
+  const time = () => {
+    var day = new Date();
+    setHh(day.getHours() * 30);
+    setMin(day.getMinutes() * deg);
+    setSec(day.getSeconds() * deg);
+  };
+
+  setInterval(time,1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="hour" style={{transform:`rotateZ(${hh+min/12}deg)`}}></div>
+      <div className="min"  style={{transform:`rotateZ(${min}deg)`}}></div>
+      <div className="sec"  style={{transform:`rotateZ(${sec}deg)`}}></div>
     </div>
   );
 }
